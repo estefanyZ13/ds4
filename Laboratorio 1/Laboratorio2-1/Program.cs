ab2ec1ddf9f185d80437642a8a97458a949cb155
estefanyZ13 <estefany.zambrano@utp.ac.pa>
2025-09-16 19:42:58 -0500

    Subiendo Laboratorio 2

---
+++ Laboratorio 1/Laboratorio2-1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+internal class Programm { 
+
+    public static void Main()
+    {
+        //asignar valor a variable estatica
+        Myclass.valor = 1;
+        Console.WriteLine(Myclass.valor);
+
+
+    }
+
+}
+public class Myclass
+{
+    //declarando variable estatica
+    public static int valor;
+
+}
+
+
+    
+    
---
+++ Laboratorio 1/Laboratorio2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Laboratori2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            //ejemplo utilizando las variables de intancia de la clase.
+            client.FirstName = "Su_Nombre";
+            client.LastName = "Su_apellido";
+            client.age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+
+        }
+    }
+
+
+
+    public class Client
+
+    {
+        //declaracion variable de intacia en clase
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int age { get; set; }
+
+        public string GetFullName()
+        {
+            //utilizando variable de intancia dentro de metodos de la clase
+            return FirstName + "" + LastName;
+        }
+    }
+}
