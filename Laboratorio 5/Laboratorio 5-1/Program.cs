2ba718eb94e23daa865456fcb666b2064d3862d4
estefanyZ13 <estefany.zambrano@utp.ac.pa>
2025-09-24 11:46:13 -0500

    Realizando el primer commit

---
+++ Laboratorio 5/Laboratorio 5-1/Program.cs
@@ -0,0 +1,42 @@
+﻿
+internal class PruebaVector1
+{
+    static void Main(string[] args)
+    {
+        PruebaVector1 pv = new PruebaVector1();
+        pv.Cargar();
+        pv.Imprimir();
+
+    }
+    private int[] sueldos;
+
+    
+    public void Cargar()
+
+    {
+        sueldos = new int[6];//inicializacion
+        for (int f = 1; f <= 5; f++)
+        {
+
+            Console.WriteLine("Ingrese sueldo del opererio" + f + ":");
+            String linea;
+            linea = Console.ReadLine();
+            sueldos[f] = int.Parse(linea);
+
+        }
+
+
+    }
+
+
+    public void Imprimir()
+    {
+        Console.WriteLine("Los 5 sueldos de los operarios son:\n");
+        for (int f = 0; f <= 5; f++)
+        {
+            Console.WriteLine("Resultado[" + f + "]: " + sueldos[f]);
+        }
+        Console.ReadKey();
+    }
+}
+
