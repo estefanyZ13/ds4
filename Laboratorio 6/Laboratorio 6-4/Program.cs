db36e9c52a73c63f4ee7508960eec70decb3b564
estefanyZ13 <estefany.zambrano@utp.ac.pa>
2025-09-24 12:49:15 -0500

    Realizacion del primer commit

---
+++ Laboratorio 6/Laboratorio 6-4/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void checkAgeB(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException();
+        }
+        else
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    static void Main(string[] args)
+    {
+        checkAgeB(15);
+    }
+}
