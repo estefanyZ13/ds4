8c2522dc46a12a0cfcf535ba6f9633b67d969c3e
estefanyZ13 <estefany.zambrano@utp.ac.pa>
2025-09-25 01:53:12 -0500

    subiendo Primer commit

---
+++ Laboratorio 7/Laboratorio 7-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Laboratorio_7;
+
+NewMethod();
+
+static void NewMethod()
+{
+    Banco banco1 = new Banco();
+    banco1.Operar();
+    banco1.DepositosTotales();
+    Console.ReadKey();
+
+}
---
+++ Laboratorio 7/Laboratorio 7-2/Dado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Laboratorio_7_1
+{
+    internal class Dado
+    {
+        private int valor;
+        private static Random aleatorio = new Random();
+
+        public void Tirar()
+        {
+            valor = aleatorio.Next(1, 7); // valores entre 1 y 6
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("El valor del dado es: " + valor);
+        }
+
+        public int RetornarValor()
+        {
+            return valor;
+        }
+    }
+}
